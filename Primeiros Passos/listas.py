f66531ecff35a385a263ea530e6b7aa49d2fa1c6
digxz <diegocastelliniramirez.dcr@gmail.com>
2025-02-11 22:39:35 -0300

    dia 08

---
+++ Primeiros Passos/listas.py
@@ -0,0 +1,12 @@
+lista = ["Diego", 10.0, 9.0, 9.5, True]
+
+# for elemento in lista:
+#     print(elemento)
+
+lista[2] = 9.5
+
+media = (lista[1] + lista[2] + lista[3])/3
+
+print(media)
+
+print(lista)
