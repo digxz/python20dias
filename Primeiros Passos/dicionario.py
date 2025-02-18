a5a22ab9c18a1723d9a5bfe328d12cf781fd8afa
digxz <diegocastelliniramirez.dcr@gmail.com>
2025-02-17 22:50:47 -0300

    retornando aos estudos

---
+++ Primeiros Passos/dicionario.py
@@ -0,0 +1,15 @@
+# dicionario = {'item01':1,
+#            'item02':2,
+#            'item03':3}
+
+# print(dicionario)
+
+cadastro = {'matricula':2000168933,
+            'dia_cadastro': 27,
+            'mes_cadastro': 9,
+            'turma': '3DA'}
+
+cadastro['turma'] = '2DA'
+cadastro['modalidade'] = 'EAD'
+ 
+print(cadastro)
--- Primeiros Passos/listas.py
+++ Primeiros Passos/listas.py
@@ -3,10 +3,18 @@ lista = ["Diego", 10.0, 9.0, 9.5, True]
 # for elemento in lista:
 #     print(elemento)
 
-lista[2] = 9.5
+# lista[2] = 9.5
 
-media = (lista[1] + lista[2] + lista[3])/3
+# media = (lista[1] + lista[2] + lista[3])/3
 
-print(media)
+# print(media)
+
+# print(lista)
+
+# print(lista[1:4])
+
+lista.remove("Diego")
+lista.remove(True)
+lista.sort()
 
 print(lista)
